e94a5b18fc95481c8a1344ca790cc1bdc72594b1
Maik20 <valya.eremin.2007@mail.ru>
2025-11-25 13:40:13 +0500

    Create 1.2.cpp

---
+++ Sem_1/1_2/1.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    float a, b;
+    cin>>a>>b;
+    if (b!=0){
+        cout<<a/b<<endl;
+    }
+    else{
+        cout<<"На 0 делить нельзя"<<endl;
+    }
+}
