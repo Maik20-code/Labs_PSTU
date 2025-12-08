b98dc76b8bb24248b1919834520ebfa1d3e0b7cb
Maik20 <valya.eremin.2007@mail.ru>
2025-12-08 15:50:11 +0500

    Create 2.14.cpp

---
+++ Sem_1/2_14/2.14.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,s;
+    cin>>n;
+    s=0;
+    while (n>0){
+        s+=n%10;
+        n/=10;
+    }
+    cout<<s<<endl;
+	return 0;
+}
