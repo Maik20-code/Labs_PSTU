66bafd0f18222a6abc1a14b47e4888095a430f39
Maik20 <valya.eremin.2007@mail.ru>
2025-12-15 16:26:56 +0500

    Create 2.22.cpp

---
+++ Sem_1/2_22/2.22.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	float a, n, max;
+	int i=2;
+	int num=1;
+	cin>>n;
+	max=sin(n+1/n);
+	while (i<=n){
+	    a=sin(n+i/n);
+	    if (a<max){
+	        max=a;
+	        num=i;
+	    }
+	    i++;
+	}
+	cout<<"Макс. элемент: "<<max<<"Его номер: "<<num<<endl;
+	return 0;
+}
