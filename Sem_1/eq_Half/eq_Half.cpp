df93e46e16e398699b45d169fc4144820a855314
Maik20 <valya.eremin.2007@mail.ru>
2025-12-22 18:09:15 +0500

    Create eq_Half.cpp

---
+++ Sem_1/eq_Half/eq_Half.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	float a, b, c;
+	float fA, fB, fC;
+	double eps=0.000001;
+	a=0;
+	b=0.85;
+	while (b-a>eps){
+	    c=(a+b)/2;
+	    fA=a-(1/(3+sin(3.6*a)));
+	    fB=b-(1/(3+sin(3.6*b)));
+	    fC=c-(1/(3+sin(3.6*c)));
+	    if (fA*fC<0){b=c;}
+	    else if (fC*fB<0){a=c;}
+	    else {cout<<"Error"<<endl;}
+	}
+	cout<<"Корни"<<a<<";"<<b<<endl;
+	return 0;
+}
