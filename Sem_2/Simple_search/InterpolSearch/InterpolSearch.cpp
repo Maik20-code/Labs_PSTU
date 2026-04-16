ec560a10aab413745cb7b7eaded84297d67e2683
Maik20 <valya.eremin.2007@mail.ru>
2026-04-16 16:54:47 +0500

    Update InterpolSearch.cpp

--- Sem_2/Simple_search/InterpolSearch/InterpolSearch.cpp
+++ Sem_2/Simple_search/InterpolSearch/InterpolSearch.cpp
@@ -14,17 +14,6 @@ int interpolSearch(int arr[], int low, int high, int x) {
 	}
 	return -1;
 }
-int interpolSearchRecursive(int arr[], int low, int high, int x) {
-	if (low > high || x<arr[low] || x>arr[high]) return -1;
-	if (low == high || arr[low] == arr[high]) {
-		if (arr[low] == x) return low;
-		return -1;
-	}
-	int pos = low + ((x - arr[low]) * (high - low)) / (arr[high] / arr[low]);
-	if (arr[pos] == x) return pos;
-	if (arr[pos] < x) interpolSearchRecursive(arr, pos+1, high, x);
-	return interpolSearchRecursive(arr, low, pos - 1, x);
-}
 void printArr(int arr[], int n) {
 	cout << "[";
 	for (int i = 0; i < n; i++) {
@@ -34,7 +23,6 @@ void printArr(int arr[], int n) {
 	cout << "]" << endl;
 }
 int main() {
-	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
 	int Arr[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
 	int n = sizeof(Arr) / sizeof(Arr[0]);
@@ -44,8 +32,5 @@ int main() {
 	int res = interpolSearch(Arr, 0, n - 1, x);
 	if (res == -1) cout << "Элемент " << x << " не найден" << endl;
 	else cout << "Элемент " << x << " найден по индексу " << res << endl;
-	int resr = interpolSearchRecursive(Arr, 0, n - 1, x);
-	if (resr == -1) cout << "Элемент " << x << " не найден" << endl;
-	else cout << "Элемент " << x << " найден по индексу " << resr << endl;
 	return 0;
 }
